68b6654c57de3f3ebb4f0066c6e8a1314abe0310
ntuthuko <ntuthukorocks@gmail.com>
2018-08-20 06:00:44 +0200

    included database script

--- DealerNetApp.Web/App_Start/FilterConfig.cs
+++ DealerNetApp.Web/App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace DealerNetApp.Web
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            //filters.Add(new RequireHttpsAttribute());
         }
     }
 }
